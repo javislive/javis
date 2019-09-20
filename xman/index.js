0f494a03715882864205664191512417fef40eca
Spencer <pallyoung@163.com>
2019-09-20 21:56:53 +0800

    archit

---
+++ xman/index.js
@@ -0,0 +1,40 @@
+const args = process.argv.slice(2);
+const cmd = args.shift();
+const shelljs = require('shelljs');
+const path = require('path');
+const metro = require('./metro');
+const fs = require('fs');
+function buildMetro(env) {
+  const config = metro(env);
+  const data = `module.exports=${JSON.stringify(config, null, '\t')}`;
+  fs.writeFileSync(
+    path.resolve(__dirname, '../metro.config.js'),
+    data,
+    'utf-8',
+  );
+}
+function start() {
+  buildMetro('debug');
+  shelljs.exec('react-native start');
+}
+function uat() {
+  buildMetro('uat');
+}
+function release() {
+  buildMetro('prod');
+}
+function relaseAndroid(buildType, falvors) {}
+function run() {
+  switch (cmd) {
+    case 'start':
+      return start();
+    case 'uat':
+      return uat();
+    case 'relase':
+      return release();
+    default:
+      break;
+  }
+}
+
+run();
---
+++ xman/metro.js
@@ -0,0 +1,45 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ */
+const path = require('path');
+const root = path.resolve(__dirname, '../');
+function pathResolve(path) {
+  return path.resolve(root, path);
+}
+function metro(env) {
+  return {
+    transformer: {
+      asyncRequireModulePath: 'react-native-typescript-transformer',
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      sourceExts: [`${env}.tsx`, 'js', 'ts', 'tsx'],
+      extraNodeModules: {
+        'react-native-ui': path.resolve(root, './app/react-native-ui'),
+        services: path.resolve(root, './app/services'),
+        context: path.resolve(root, './app/context'),
+        native: path.resolve(root, './app/native'),
+        utils: path.resolve(root, './app/utils'),
+        assets: path.resolve(root, './app/assets'),
+        themes: path.resolve(root, './app/themes'),
+        components: path.resolve(root, './app/components'),
+        router: path.resolve(root, './app/router'),
+        BuildConfig: path.resolve(root, './app/BuildConfig'),
+        pages: path.resolve(root, './app/pages'),
+        store: path.resolve(root, './app/store'),
+        controller: path.resolve(root, './app/controller'),
+        state: path.resolve(root, './app/state'),
+        celtics: path.resolve(root, './app/celtics'),
+      },
+    },
+    projectRoot: path.resolve(root),
+  };
+}
+module.exports = metro;
